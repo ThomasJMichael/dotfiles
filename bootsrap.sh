18a3c14d6221296ad44cda5dd3b938569d3f5cb8
Jacob Michael <thomasjmichael@outlook.com>
2023-08-21 22:39:55 +0000

    Adding --install-neovim command line argument to bootstrap.sh

--- bootsrap.sh
+++ bootsrap.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 
+INSTALL_NEOVIM=false
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --install-neovim)
+            INSTALL_NEOVIM=true
+            shift
+            ;;
+        *)
+            echo "Unknown parameter: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+if $INSTALL_NEOVIM; then
+    echo "Installing Neovim..."
+    ./install_neovim.sh
+else
+    echo "Skipping Neovim installation."
+fi
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+echo "Linking Dotfiles!"
 # ln -sf "$DIR/.bashrc" ~/
 ln -s ~/dotfiles/nvim ~/.config/nvim
 
