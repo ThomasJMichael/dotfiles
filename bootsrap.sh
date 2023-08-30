78b26cd476a876127d66465a166c61eda7228952
Jacob Michael <thomasjmichael@outlook.com>
2023-08-29 19:50:12 -0500

    Update bootsrap.sh

--- bootsrap.sh
+++ bootsrap.sh
@@ -28,5 +28,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "Linking Dotfiles!"
 # ln -sf "$DIR/.bashrc" ~/
 ln -s ~/dotfiles/nvim ~/.config/nvim
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 echo "Dotfiles linked!"
